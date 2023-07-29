0bfd74952b8f1cbce709e05c1e1b235f4b90e472
Mathieu Barbin <mathieu.barbin@gmail.com>
2023-07-29 16:19:27 +0200

    comment rewrap

--- src/automaton.mli
+++ src/automaton.mli
@@ -1,31 +1,27 @@
 open! Core
 
-(** To solve the game, we create a big automaton that encodes the
-    transitions between all possible status lines. Once that automaton
-    is computed, the search for a solution boils down to finding a
-    particular path within it : a path that starts from the initial
-    state and that leads to a state where the location of the bunny is
-    known.
+(** To solve the game, we create a big automaton that encodes the transitions
+    between all possible status lines. Once that automaton is computed, the
+    search for a solution boils down to finding a particular path within it :
+    a path that starts from the initial state and that leads to a state where
+    the location of the bunny is known.
 
-    The vertices of the automaton are all the different status lines
-    that may be created. The edges are directed, and labelled with and
-    index of the box. An edge [b] from state [s1 -> s2] means that
-    given the information [s1], if we choose to open the box [b] and
-    the bunny is not there, and then it moves, then we're left in a
-    state with known information [s2].
+    The vertices of the automaton are all the different status lines that may be
+    created. The edges are directed, and labelled with and index of the box. An
+    edge [b] from state [s1 -> s2] means that given the information [s1], if we
+    choose to open the box [b] and the bunny is not there, and then it moves,
+    then we're left in a state with known information [s2].
 
-    Not all status lines are reachable from the initial state, meaning
-    some vertices have no incoming edges. *)
+    Not all status lines are reachable from the initial state, meaning some
+    vertices have no incoming edges. *)
 
 type t
 
-(** Compute the complete automaton with its vertices and edges for a
-    given size. *)
+(** Compute the complete automaton with its vertices and edges for a given size. *)
 val create : size:int -> t
 
-(** Returns the edges that are originating from the vertex with the
-    given code. For each edge we return its label along with its
-    targeting vertex. *)
+(** Returns the edges that are originating from the vertex with the given code.
+    For each edge we return its label along with its targeting vertex. *)
 val edges : t -> code:int -> (int * Status_line.t) list
 
 (** Return the edges that are targeting the vertex with the given code. *)
@@ -41,9 +37,9 @@ module Step : sig
   [@@deriving sexp_of]
 end
 
-(** Test a candidate solution, and returns its human readable
-    execution trace. The bunny is caught at the end of the given
-    sequence if it is indeed a valid solution. *)
+(** Test a candidate solution, and returns its human readable execution trace.
+    The bunny is caught at the end of the given sequence if it is indeed a
+    valid solution. *)
 val execute_sequence : t -> sequence:int list -> Step.t list
 
 module Solution : sig
@@ -54,8 +50,7 @@ module Solution : sig
   [@@deriving sexp_of]
 end
 
-(** Find a few sequences that catch the bunny, ordered by increasing
-    number of steps. This function does not return all possible
-    solutions, rather only a few among those which minimizes the number
-    of steps required. *)
+(** Find a few sequences that catch the bunny, ordered by increasing number of
+    steps. This function does not return all possible solutions, rather only a
+    few among those which minimizes the number of steps required. *)
 val catch_the_bunny : t -> Solution.t list
--- src/status_line.mli
+++ src/status_line.mli
@@ -1,29 +1,27 @@
 open! Core
 
-(** A status line indicates the columns where the bunny may be located
-    on that line. As the game goes on and more information is gathered,
-    we're hoping that the set of possible locations will be decreased
-    down to a single column, so that we can catch the bunny at that
-    location. *)
+(** A status line indicates the columns where the bunny may be located on that
+    line. As the game goes on and more information is gathered, we're hoping
+    that the set of possible locations will be decreased down to a single
+    column, so that we can catch the bunny at that location. *)
 
 type t [@@deriving compare, equal, sexp_of]
 
 (** Status lines are encoded with an int between 0 and (2**size - 1). *)
 val code : t -> int
 
-(** Create a new status line with the given size and code. Raises when
-    called with a code that exceeds the code range. *)
+(** Create a new status line with the given size and code. Raises when called
+    with a code that exceeds the code range. *)
 val create : size:int -> code:int -> t
 
-(** Update the information known upon witnessing that the bunny is not
-    located at a given box that was just opened. *)
+(** Update the information known upon witnessing that the bunny is not located
+    at a given box that was just opened. *)
 val remove : t -> index:int -> t
 
-(** Given the information we currently know, return the new
-    information we will know after the bunny moves from its current
-    location. *)
+(** Given the information we currently know, return the new information we will
+    know after the bunny moves from its current location. *)
 val move : t -> t
 
-(** If there is only one single column where the bunny may currently
-    be located, return that column, and [None] otherwise. *)
+(** If there is only one single column where the bunny may currently be located,
+    return that column, and [None] otherwise. *)
 val catch_the_bunny : t -> int option
