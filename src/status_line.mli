591e29534a809468220adef98d0d92dd19c7965e
Mathieu Barbin <mathieu.barbin@gmail.com>
2023-07-29 16:18:31 +0200

    upgrade ocamlformat to 0.26.0

--- src/status_line.mli
+++ src/status_line.mli
@@ -1,10 +1,10 @@
 open! Core
 
 (** A status line indicates the columns where the bunny may be located
-   on that line. As the game goes on and more information is gathered,
-   we're hoping that the set of possible locations will be decreased
-   down to a single column, so that we can catch the bunny at that
-   location. *)
+    on that line. As the game goes on and more information is gathered,
+    we're hoping that the set of possible locations will be decreased
+    down to a single column, so that we can catch the bunny at that
+    location. *)
 
 type t [@@deriving compare, equal, sexp_of]
 
@@ -12,18 +12,18 @@ type t [@@deriving compare, equal, sexp_of]
 val code : t -> int
 
 (** Create a new status line with the given size and code. Raises when
-   called with a code that exceeds the code range. *)
+    called with a code that exceeds the code range. *)
 val create : size:int -> code:int -> t
 
 (** Update the information known upon witnessing that the bunny is not
-   located at a given box that was just opened. *)
+    located at a given box that was just opened. *)
 val remove : t -> index:int -> t
 
 (** Given the information we currently know, return the new
-   information we will know after the bunny moves from its current
-   location. *)
+    information we will know after the bunny moves from its current
+    location. *)
 val move : t -> t
 
 (** If there is only one single column where the bunny may currently
-   be located, return that column, and [None] otherwise. *)
+    be located, return that column, and [None] otherwise. *)
 val catch_the_bunny : t -> int option
